98cd4b2b416af5fb72e770560a24582ecfa3ae20
Gizpo <ulbrich.f@web.de>
2018-04-17 01:19:33 +0200

    add bindings for uia_core

---
+++ uia_core/src/main/java/eu/doppel_helix/jna/tlbcodegenerator/uia_core/AutomationElementMode.java
@@ -0,0 +1,27 @@
+
+package eu.doppel_helix.jna.tlbcodegenerator.uia_core;
+
+import com.sun.jna.platform.win32.COM.util.IComEnum;
+
+public enum AutomationElementMode implements IComEnum {
+    
+    /**
+     * (0)
+     */
+    AutomationElementMode_None(0),
+    
+    /**
+     * (1)
+     */
+    AutomationElementMode_Full(1),
+    ;
+
+    private AutomationElementMode(long value) {
+        this.value = value;
+    }
+    private long value;
+
+    public long getValue() {
+        return this.value;
+    }
+}
---
+++ uia_core/src/main/java/eu/doppel_helix/jna/tlbcodegenerator/uia_core/WindowVisualState.java
@@ -0,0 +1,32 @@
+
+package eu.doppel_helix.jna.tlbcodegenerator.uia_core;
+
+import com.sun.jna.platform.win32.COM.util.IComEnum;
+
+public enum WindowVisualState implements IComEnum {
+    
+    /**
+     * (0)
+     */
+    WindowVisualState_Normal(0),
+    
+    /**
+     * (1)
+     */
+    WindowVisualState_Maximized(1),
+    
+    /**
+     * (2)
+     */
+    WindowVisualState_Minimized(2),
+    ;
+
+    private WindowVisualState(long value) {
+        this.value = value;
+    }
+    private long value;
+
+    public long getValue() {
+        return this.value;
+    }
+}
