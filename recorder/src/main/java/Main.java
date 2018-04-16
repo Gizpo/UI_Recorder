93d079d62618f0fb1f0444364d373e8f6a021cf2
Felix Ulbrich <ulbrich.f@web.de>
2018-04-17 01:27:53 +0200

    hopefully last fix for now

--- recorder/src/main/java/Main.java
+++ recorder/src/main/java/Main.java
@@ -1,8 +1,5 @@
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
-import mmarquee.automation.AutomationException;
-import mmarquee.automation.pattern.PatternNotFoundException;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
