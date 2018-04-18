fb879e7830f655918fa86801c894bdc5df79edf6
Felix Ulbrich <ulbrich.f@web.de>
2018-04-18 06:38:44 +0200

    test dependency stuff

--- recorder/src/main/java/UI_Automator.java
+++ recorder/src/main/java/UI_Automator.java
@@ -1,7 +1,6 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 
 public class UI_Automator {
    final Logger logger = LoggerFactory.getLogger(UI_Automator.class);
