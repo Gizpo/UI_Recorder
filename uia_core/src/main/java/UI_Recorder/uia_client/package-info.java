4d05ae72d79ef86c02bd53af22b808235e414493
Felix Ulbrich <ulbrich.f@web.de>
2018-04-19 21:38:25 +0200

    clean up

--- uia_core/src/main/java/UI_Recorder/uia_client/package-info.java
+++
@@ -1,7 +0,0 @@
-
-/**
- * UIAutomationClient
- * <p>uuid({944DE083-8FB8-45CF-BCB7-C477ACB2F897})</p>
- * <p>version(1.0)</p>
- */
-package UI_Recorder.uia_client;
--- uia_core/src/main/java/UI_Recorder/uia_core/package-info.java
+++
@@ -1,7 +0,0 @@
-
-/**
- * UIA
- * <p>uuid({930299CE-9965-4DEC-B0F4-A54848D4B667})</p>
- * <p>version(1.0)</p>
- */
-package UI_Recorder.uia_core;
