55191b2c9b4443dde4030cbcf6817e0bae6996e0
Gizpo <ulbrich.f@web.de>
2018-04-19 21:39:53 +0200

    build client

---
+++ uia_core/src/main/java/UI_Recorder/uia/IUIAutomationChangesEventHandler.java
@@ -0,0 +1,30 @@
+
+package UI_Recorder.uia;
+
+import com.sun.jna.platform.win32.COM.util.annotation.ComInterface;
+import com.sun.jna.platform.win32.COM.util.annotation.ComMethod;
+import com.sun.jna.platform.win32.COM.util.annotation.ComProperty;
+import com.sun.jna.platform.win32.COM.util.IDispatch;
+import com.sun.jna.platform.win32.COM.util.IUnknown;
+import com.sun.jna.platform.win32.COM.util.IRawDispatchHandle;
+import com.sun.jna.platform.win32.Variant.VARIANT;
+
+/**
+ * <p>uuid({58EDCA55-2C3E-4980-B1B9-56C17F27A2A0})</p>
+ */
+@ComInterface(iid="{58EDCA55-2C3E-4980-B1B9-56C17F27A2A0}")
+public interface IUIAutomationChangesEventHandler extends IUnknown, IRawDispatchHandle {
+    /**
+     * <p>id(0x60010000)</p>
+     * <p>vtableId(3)</p>
+     * @param sender [in] {@code IUIAutomationElement}
+     * @param uiaChanges [in] {@code UiaChangeInfo}
+     * @param changesCount [in] {@code Integer}
+     */
+    @ComMethod(name = "HandleChangesEvent", dispId = 0x60010000)
+    com.sun.jna.platform.win32.WinNT.HRESULT HandleChangesEvent(IUIAutomationElement sender,
+            UiaChangeInfo uiaChanges,
+            Integer changesCount);
+            
+    
+}
---
+++ uia_core/src/main/java/UI_Recorder/uia/IUIAutomationStructureChangedEventHandler.java
@@ -0,0 +1,30 @@
+
+package UI_Recorder.uia;
+
+import com.sun.jna.platform.win32.COM.util.annotation.ComInterface;
+import com.sun.jna.platform.win32.COM.util.annotation.ComMethod;
+import com.sun.jna.platform.win32.COM.util.annotation.ComProperty;
+import com.sun.jna.platform.win32.COM.util.IDispatch;
+import com.sun.jna.platform.win32.COM.util.IUnknown;
+import com.sun.jna.platform.win32.COM.util.IRawDispatchHandle;
+import com.sun.jna.platform.win32.Variant.VARIANT;
+
+/**
+ * <p>uuid({E81D1B4E-11C5-42F8-9754-E7036C79F054})</p>
+ */
+@ComInterface(iid="{E81D1B4E-11C5-42F8-9754-E7036C79F054}")
+public interface IUIAutomationStructureChangedEventHandler extends IUnknown, IRawDispatchHandle {
+    /**
+     * <p>id(0x60010000)</p>
+     * <p>vtableId(3)</p>
+     * @param sender [in] {@code IUIAutomationElement}
+     * @param changeType [in] {@code StructureChangeType}
+     * @param runtimeId [in] {@code Integer}
+     */
+    @ComMethod(name = "HandleStructureChangedEvent", dispId = 0x60010000)
+    com.sun.jna.platform.win32.WinNT.HRESULT HandleStructureChangedEvent(IUIAutomationElement sender,
+            StructureChangeType changeType,
+            Integer runtimeId);
+            
+    
+}
