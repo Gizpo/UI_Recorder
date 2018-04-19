68293d20acded10781f3d209791b93d5c77f2366
Gizpo <ulbrich.f@web.de>
2018-04-19 21:42:21 +0200

    build crossbitnesshook

--- uia_core/src/main/java/UI_Recorder/uia/package-info.java
+++ uia_core/src/main/java/UI_Recorder/uia/package-info.java
@@ -1,10 +1,10 @@
 
 /**
- * UIAutomationBlockingCoreLib
+ * UIAutomationCrossBitnessHookLib
  *
- * <p>UIAutomation BlockingCore 1.0 Type Library</p>
+ * <p>UIAutomation CrossBitnessHook 1.0 Type Library</p>
  *
- * <p>uuid({EA39B853-5769-4937-8ECE-736DE4F469BC})</p>
+ * <p>uuid({8ACC2016-04A3-4343-B8E1-1870E35D6A41})</p>
  * <p>version(1.0)</p>
  */
 package UI_Recorder.uia;
