8bbb4110284fcbf1ef72e05887944dc9f1ea6acf
Gizpo <ulbrich.f@web.de>
2018-04-22 12:10:54 +0200

    build core

---
+++ uia_core/src/main/java/UI_Recorder/uia_core/IInvokeProvider.java
@@ -0,0 +1,18 @@
+
+package UI_Recorder.uia_core;
+
+import com.sun.jna.platform.win32.COM.util.annotation.ComInterface;
+import com.sun.jna.platform.win32.COM.util.annotation.ComMethod;
+import com.sun.jna.platform.win32.COM.util.annotation.ComProperty;
+import com.sun.jna.platform.win32.COM.util.IDispatch;
+import com.sun.jna.platform.win32.COM.util.IUnknown;
+import com.sun.jna.platform.win32.COM.util.IRawDispatchHandle;
+import com.sun.jna.platform.win32.Variant.VARIANT;
+
+/**
+ * <p>uuid({54FCB24B-E18E-47A2-B4D3-ECCBE77599A2})</p>
+ */
+@ComInterface(iid="{54FCB24B-E18E-47A2-B4D3-ECCBE77599A2}")
+public interface IInvokeProvider extends IUnknown, IRawDispatchHandle {
+    
+}
---
+++ uia_core/src/main/java/UI_Recorder/uia_core/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * UIA
+ * <p>uuid({930299CE-9965-4DEC-B0F4-A54848D4B667})</p>
+ * <p>version(1.0)</p>
+ */
+package UI_Recorder.uia_core;
