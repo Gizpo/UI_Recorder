148060d261e88a9db070c7a05cf021a90c57a0d4
Gizpo <ulbrich.f@web.de>
2018-04-22 13:22:38 +0200

    generate uia_client

---
+++ uia_core/src/main/java/UI_Recorder/uia_core/NotificationKind.java
@@ -0,0 +1,42 @@
+
+package UI_Recorder.uia_core;
+
+import com.sun.jna.platform.win32.COM.util.IComEnum;
+
+public enum NotificationKind implements IComEnum {
+    
+    /**
+     * (0)
+     */
+    NotificationKind_ItemAdded(0),
+    
+    /**
+     * (1)
+     */
+    NotificationKind_ItemRemoved(1),
+    
+    /**
+     * (2)
+     */
+    NotificationKind_ActionCompleted(2),
+    
+    /**
+     * (3)
+     */
+    NotificationKind_ActionAborted(3),
+    
+    /**
+     * (4)
+     */
+    NotificationKind_Other(4),
+    ;
+
+    private NotificationKind(long value) {
+        this.value = value;
+    }
+    private long value;
+
+    public long getValue() {
+        return this.value;
+    }
+}
